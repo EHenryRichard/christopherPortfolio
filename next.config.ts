c44219af9e0324dc79144077fca0c280a2671664
EHenryRichard <rickemzy5@gmail.com>
2026-01-31 13:01:25 +0100

    free

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // Ensure there are no typos and you are on Next.js 14.3+ or 15
+    allowedDevOrigins: ['http://10.88.54.99:3000', 'http://localhost:3000'],
+  },
 };
 
 export default nextConfig;
