1c3ca503fecd8976f10f1681f5b2e76fcaac62e9
EHenryRichard <rickemzy5@gmail.com>
2026-01-31 13:09:38 +0100

    free

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    // This allows your specific device IP to access the dev server
-    allowedDevOrigins: ['10.88.54.99'],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
